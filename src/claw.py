75aded315f81c86942396a1e7c298c979ab57974
Marko Lalovic <marko@markolalovic.com>
2021-09-14 11:02:00 +0200

    fixed delta/2, sigma=0 sampling

--- src/claw.py
+++ src/claw.py
@@ -5,7 +5,7 @@ embedded in 2-dimensional Euclidean space that is K_1,3 or Claw
 glued together in such a way it is hard to distinguish two edges
 because they are too close in the embedding space; see (Lecci et al., 2014)
     https://jmlr.csail.mit.edu/papers/volume15/lecci14a/lecci14a.pdf
-chapter 3.1.
+Section 3.1.
 """
 
 import matplotlib.pyplot as plt
@@ -36,7 +36,7 @@ class Claw:
         * tau is the radius of each circle;
 
     '''
-    def __init__(self, title='', n=1000):
+    def __init__(self, n=1000):
         self.tau = np.sqrt(2)
         self.n = n
         self.n1 = self.n + 2 # plus 2, because we remove the boundary points
@@ -164,7 +164,6 @@ class Claw:
     def plot_parts(self, plot_construction=False, savefig=False):
         ''' Plots the parts of embedded graph K_1,3 construction. '''
         self.plot(plot_construction)
-        self.ax.set_title('Parts of embedded graph $K_{1,3}$ construction')
 
         boundary = self.boundary_points()
         boundary_np = np.array(boundary)
@@ -183,67 +182,64 @@ class Claw:
         self.ax.scatter(arc_e2_np[:, 0], arc_e2_np[:, 1], color='red', s=50)
 
         if savefig:
-            self.fig.savefig('../figures/claw_parts.png', dpi=300)
-            os.system('convert ../figures/claw_parts.png -trim ../figures/claw_parts.png')
+            self.fig.savefig('../figures/claw/claw_parts.png', dpi=300)
+            os.system('convert ../figures/claw/claw_parts.png -trim \
+                ../figures/claw/claw_parts.png')
 
     def plot_claw(self, savefig=False):
         self.plot()
-        self.ax.set_title('Embedded graph $K_{1,3}$')
 
         points = np.array(self.points())
         self.ax.scatter(points[:, 0], points[:, 1], marker='.', color='black')
 
         if savefig:
-            self.fig.savefig('../figures/claw.png', dpi=300)
-            os.system('convert ../figures/claw.png -trim ../figures/claw.png')
+            self.fig.savefig('../figures/claw/claw.png', dpi=300)
+            os.system('convert ../figures/claw/claw.png -trim \
+                ../figures/claw/claw.png')
 
     def get_sample_points(self, nn):
         points = self.points()
         return sample(points, nn)
 
-    def plot_sample(self, sample_points, sigma=0, savefig=False):
+    def plot_sample(self, sample_points, delta, savefig=False):
         self.plot()
 
-        dense_sample = self.is_dense(sample_points, sigma)
-        if dense_sample:
-            self.ax.set_title('Dense sample of points from embedded graph $K_{1,3}$')
-        else:
-            self.ax.set_title('Not dense sample of points from embedded graph $K_{1,3}$')
+        dense_sample = self.is_dense(sample_points, delta)
 
         sample_points_np = np.array(sample_points)
         self.ax.scatter(sample_points_np[:, 0], sample_points_np[:, 1],
                         facecolors='k', edgecolors='k', alpha=0.9, s=50)
 
-        if sigma > 0:
+        if delta > 0:
             for sample_point in sample_points:
                 circle = patches.Circle((sample_point[0], sample_point[1]),
-                                        sigma,
+                                        delta/2,
                                         facecolor=(0.5, 0.5, 0.5),
                                         edgecolor=(0.0, 0.0, 0.0),
+                                        linestyle='--',
                                         alpha=0.1)
                 self.ax.add_patch(circle)
 
         if savefig:
             if dense_sample:
-                self.fig.savefig('../figures/claw_dense_sample.png', dpi=300)
-                os.system('convert ../figures/claw_dense_sample.png -trim  \
-                    ../figures/claw_dense_sample.png')
+                self.fig.savefig('../figures/claw/claw_dense_sample.png', dpi=300)
+                os.system('convert ../figures/claw/claw_dense_sample.png -trim \
+                    ../figures/claw/claw_dense_sample.png')
             else:
-                self.fig.savefig('../figures/claw_not_dense_sample.png', dpi=300)
-                os.system('convert ../figures/claw_not_dense_sample.png -trim  \
-                    ../figures/claw_not_dense_sample.png')
+                self.fig.savefig('../figures/claw/claw_not_dense_sample.png', dpi=300)
+                os.system('convert ../figures/claw/claw_not_dense_sample.png -trim \
+                    ../figures/claw/claw_not_dense_sample.png')
 
-    def is_dense(self, sample_points, sigma):
+    def is_dense(self, sample_points, delta):
         points_EG = self.points()
         for point_EG in points_EG:
-            if self.not_covered(point_EG, sample_points, sigma):
+            if self.not_covered(point_EG, sample_points, delta):
                 return False
         return True
 
-    def not_covered(self, pointEG, sample_points, sigma):
+    def not_covered(self, pointEG, sample_points, delta):
         for sample_point in sample_points:
-            # TODO: including the boundary?
-            if self.distance(sample_point, pointEG) < sigma:
+            if self.distance(sample_point, pointEG) < delta/2:
                 return False
         return True
 
@@ -253,24 +249,46 @@ class Claw:
         p2 = np.array([p2[0], p2[1]])
         return np.linalg.norm(p1 - p2)
 
+    def save_sample(self, sample_points, file_name):
+        np.savetxt('../data/claw/' + file_name + '.out',
+                   np.array(sample_points),
+                   delimiter=',')
+
+    def load_sample(self, file_name):
+        sample_points_np = np.loadtxt('../data/claw/' + file_name + '.out',
+                          delimiter=',')
+        return [tuple(pt) for pt in sample_points_np]
+
 if __name__ == "__main__":
     np.random.seed(2)
 
     savefig = True # set false for faster plotting
 
-    claw = Claw(n=10)
-    claw.plot_parts(plot_construction=True, savefig=savefig)
-    plt.show()
-
+    ## Visualize the worst case claw G
     claw = Claw()
     claw.plot_claw(savefig=savefig)
     plt.show()
 
-    sigma = 0.1
-    sample_points = claw.get_sample_points(10)
-    claw.plot_sample(sample_points, sigma, savefig=savefig)
+    ## Construction
+    claw = Claw(n=10)
+    claw.plot_parts(plot_construction=True, savefig=savefig)
     plt.show()
 
-    sample_points = claw.get_sample_points(100)
-    claw.plot_sample(sample_points, sigma, savefig=savefig)
+    ## Sampling
+    delta = 0.2
+
+    # NOT delta/2-dense sample in G
+    sample_points = claw.get_sample_points(10)
+    print(claw.is_dense(sample_points, delta))
+    claw.plot_sample(sample_points, delta, savefig=savefig)
     plt.show()
+
+    # delta/2-dense sample in G
+    sample_points = claw.get_sample_points(30)
+    print(claw.is_dense(sample_points, delta))
+    claw.plot_sample(sample_points, delta, savefig=savefig)
+    plt.show()
+
+    # save and load the sample
+    claw.save_sample(sample_points, 'claw_dense_sample_30_points')
+    # claw.load_sample('claw_dense_sample_30_points')
